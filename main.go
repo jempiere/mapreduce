a2e0a21cc676c547cf97e36c8698a8bbd936490e
Jayson Bunnell <jaysonbunnell@gmail.com>
2024-04-26 16:15:31 -0600

    Restructure

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package mapreduce
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Printf("Hello, World!")
+	fmt.Printf("Hello, World!\n")
+	localhost()
+}
+
+func localhost() {
+	address := "localhost:8080"
+	tempdir := "./files"
+
+	go func() {
+		http.Handle("/data/", http.StripPrefix("/data", http.FileServer(http.Dir(tempdir))))
+		if err := http.ListenAndServe(address, nil); err != nil {
+			log.Printf("Err in HTTP Server for %s: %v", address, err)
+		}
+	}()
 }
